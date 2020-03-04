3b9b1549dff9b4d1b3d80643c9407ffeb6ede8ee
zuko1623 <zuko1623@gmail.com>
2020-03-04 17:11:16 +0300

    fix: case insensitive feature: output with count

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import json
 import xml.etree.ElementTree as ET
 
 
-def get_long_words(s):
+def get_long_words_trans_to_lower(s):
     words = []
     for w in s.split():
         if len(w) > 6:
-            words.append(w)
+            words.append(w.lower())
     return words
 
 
@@ -19,7 +19,7 @@ def get_top_words(words, top_count=10):
         else:
             words_count[i] = 1
     top_words_with_count = dict(sorted(words_count.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)[:top_count])
-    return list(top_words_with_count.keys())
+    return top_words_with_count
 
 
 def newsreader_top_json(filename):
@@ -27,7 +27,7 @@ def newsreader_top_json(filename):
         news = json.load(f)
         all_words = []
         for i in news['rss']['channel']['items']:
-            all_words += get_long_words(i['description'])
+            all_words += get_long_words_trans_to_lower(i['description'])
     return get_top_words(all_words)
 
 
@@ -35,9 +35,17 @@ def newsreader_top_xml(filename):
     all_words = []
     tree = ET.parse(filename)
     for item in tree.findall('channel/item'):
-        all_words += get_long_words(item.find('description').text)
+        all_words += get_long_words_trans_to_lower(item.find('description').text)
     return get_top_words(all_words)
 
 
-print(f"Top 10 from json: {newsreader_top_json('news_source/newsafr.json')}")
-print(f"Top 10 from xml: {newsreader_top_xml('news_source/newsafr.xml')}")
+def print_word_count(title_text, wc):
+    print(title_text)
+    for word, count in wc:
+        print(f"{word} - {count}")
+
+
+print_word_count("Top 10 from json (word - count)", newsreader_top_json('news_source/newsafr.json').items())
+print("*" * 10)
+print_word_count("Top 10 from xml (word - count)", newsreader_top_xml('news_source/newsafr.xml').items())
+
